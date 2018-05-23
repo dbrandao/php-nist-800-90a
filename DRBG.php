a87d5b03584b94bb9cc281fa8940728b90c23602
Eduardo Vasconcelos <esmev@protonmail.ch>
2018-05-22 21:01:34 -0300

    DRBG and test script skeletons

---
+++ DRBG.php
@@ -0,0 +1,28 @@
+<?php
+
+class DRBG {
+
+    const
+        ALGO_HMAC = 0,
+        ALGO_HASH = 1,
+        ALGO_CTR = 2
+    ;
+    
+    private $algo;
+    
+    function __construct($algo) {
+        if ($algo == self::ALGO_HMAC) {
+            $this->algo = self::ALGO_HMAC;
+        } else if ($algo == self::ALGO_HASH) {
+            $this->algo = self::ALGO_HASH;
+        } else if ($algo == self::ALGO_CTR) {
+            $this->algo = self::ALGO_CTR;
+        } else {
+            throw new Exception('Invalid algorithm.');
+        }
+    }
+
+}
+
+?>
+
--- test.php
+++ test.php
@@ -1,4 +1,43 @@
 <?php
 
+require_once 'DRBG.php';
+
+function testDRBG($drbg, $testFileHome, $testFile) {
+
+    if (substr($testFileHome, -1) != '/') {
+        $testFileHome = $testFileHome . '/';
+    }
+    
+    // Debug
+    //echo 'Test file is at: ', $testFileHome . $testFile, "\n";
+}
+
+// Test file home
+$testFileHome = $argv[1];
+
+// Test files
+$hmacTestFile = 'HMAC_DRBG.rsp';
+//$hashTestFile = 'HASH_DRBG.rsp';
+//$ctrTestFile = 'CTR_DRBG.rsp';
+
+// DRBG instances
+try {
+    // Test exception handling
+    //$hmacDrbg = new DRBG(5); // OK, it works.
+    
+    // Instantiate DRBG objects
+    $hmacDrbg = new DRBG(DRBG::ALGO_HMAC);
+    //$hashDrbg = new DRBG(DRBG::ALGO_HASH);
+    //$ctrDrbg = new DRBG(DRBG::ALGO_CTR);
+    
+    // Test DRBG instances
+    testDRBG($hmacDrbg, $testFileHome, $hmacTestFile);
+    //testDRBG($hashDrbg, $testFileHome, $hashTestFile);
+    //testDRBG($ctrDrbg, $testFileHome, $ctrTestFile);
+
+} catch (Exception $e) {
+    echo 'Caught exception: ', $e->getMessage(), "\n";
+}
+
 ?>
 
