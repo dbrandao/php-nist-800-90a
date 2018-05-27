4a3273ae0e502babf9f1d5fd5267545c798ba41b
Eduardo Vasconcelos <esmev@protonmail.ch>
2018-05-26 23:35:08 -0300

    All done HMAC

--- test.php
+++ test.php
@@ -2,7 +2,14 @@
 
 require_once 'DRBG.php';
 
-function testHmacDrbg($testFileHome, $testFile) {
+const
+    __NO_RESEED = 1,
+    __PR_FALSE = 2,
+    __PR_TRUE = 3,
+    __HMAC_TEST_FILE = 'HMAC_DRBG.rsp'
+    ;
+
+function testHmacDrbg($testFileHome, $testFile, $__pr=__NO_RESEED) {
 
     if (substr($testFileHome, -1) != '/') {
         $testFileHome = $testFileHome . '/';
@@ -33,8 +40,18 @@ function testHmacDrbg($testFileHome, $testFile) {
                 fscanf($handle, "EntropyInput = %s", $entropyInput);
                 fscanf($handle, "Nonce = %s", $nonce);
                 fscanf($handle, "PersonalizationString = %s", $personalizationString);
+                if ($__pr == __PR_FALSE) {
+                    fscanf($handle, "EntropyInputReseed = %s", $entropyInputReseed);
+                    fscanf($handle, "AdditionalInputReseed = %s", $additionalInputReseed);
+                }
                 fscanf($handle, "AdditionalInput = %s", $additionalInput0);
+                if ($__pr == __PR_TRUE) {
+                    fscanf($handle, "EntropyInputPR = %s", $entropyInputPR0);
+                }
                 fscanf($handle, "AdditionalInput = %s", $additionalInput1);
+                if ($__pr == __PR_TRUE) {
+                    fscanf($handle, "EntropyInputPR = %s", $entropyInputPR1);
+                }
                 fscanf($handle, "ReturnedBits = %s", $returnedBits);
                 
                 $entropyInput = trim($entropyInput);
@@ -44,20 +61,35 @@ function testHmacDrbg($testFileHome, $testFile) {
                 } else {
                     $personalizationString = trim($personalizationString);
                 }
+                if ($__pr == __PR_FALSE) {
+                    $entropyInputReseed = trim($entropyInputReseed);
+                    $additionalInputReseed = trim($additionalInputReseed);
+                }
                 if ($additionalInputLen == 0) {
                     $additionalInput0 = NULL;
                     $additionalInput1 = NULL;
+                    $additionalInputReseed = NULL;
                 } else {
                     $additionalInput0 = trim($additionalInput0);
                     $additionalInput1 = trim($additionalInput1);
+                }
+                if ($__pr == __PR_TRUE) {
+                    $entropyInputPR0 = trim($entropyInputPR0);
+                    $entropyInputPR1 = trim($entropyInputPR1);                    
+                } else {
+                    $entropyInputPR0 = NULL;
+                    $entropyInputPR1 = NULL;
                 }
                 $returnedBits = trim($returnedBits);
                 
                 $countTest = $countTest + 1;
                    
                 $hmacDrbg = new HMAC_DRBG($predictionResistance, $personalizationString, TRUE, $entropyInput, $nonce);
-                $hmacDrbg->generate($returnedBitsLen, $additionalInput0, $predictionResistance);
-                if ($hmacDrbg->generate($returnedBitsLen, $additionalInput1, $predictionResistance) == hex2bin($returnedBits)) {
+                if ($__pr == __PR_FALSE) {
+                    $hmacDrbg->reseed($additionalInputReseed, $entropyInputReseed);
+                }
+                $hmacDrbg->generate($returnedBitsLen, $additionalInput0, $predictionResistance, $entropyInputPR0);
+                if ($hmacDrbg->generate($returnedBitsLen, $additionalInput1, $predictionResistance, $entropyInputPR1) == hex2bin($returnedBits)) {
                     $countPass = $countPass + 1;
                 } else {
                     echo 'Test #' . $countTest . ' expected: ' . $returnedBits . "\n\n";
@@ -77,15 +109,21 @@ function testHmacDrbg($testFileHome, $testFile) {
     }
 }
 
-// Test file home
-$testFileHome = $argv[1];
-
-// Test files
-$hmacTestFile = 'HMAC_DRBG.rsp';
+// Test file homes
+$testFileHomeNoReseed = $argv[1];
+$testFileHomePrFalse = $argv[2];
+$testFileHomePrTrue = $argv[3];
 
 try {
 
-    testHmacDrbg($testFileHome, $hmacTestFile);
+    echo 'Testing w/ no_reseed: ';
+    testHmacDrbg($testFileHomeNoReseed, __HMAC_TEST_FILE, __NO_RESEED); // _no_reseed
+    
+    echo 'Testing w/ pr_false: ';
+    testHmacDrbg($testFileHomePrFalse, __HMAC_TEST_FILE, __PR_FALSE); // _pr_false
+    
+    echo 'Testing w/ pr_true: ';
+    testHmacDrbg($testFileHomePrTrue, __HMAC_TEST_FILE, __PR_TRUE); // _pr_true
 
 } catch (Exception $e) {
     
