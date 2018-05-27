b4ceb02eae729247207afb1e97fb5b3441a255c9
dbrandao <dbrdem@hotmail.com>
2018-05-27 00:25:44 -0300

    Update run_tests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -5,4 +5,4 @@
 # Diogo Brandão, dbrdem@hotmail.com
 
 php test.php test/drbgtestvectors/drbgvectors_no_reseed/ test/drbgtestvectors/drbgvectors_pr_false/ test/drbgtestvectors/drbgvectors_pr_true/
-
+ 
